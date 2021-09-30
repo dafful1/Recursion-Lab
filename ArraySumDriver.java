c6aa3213a5884cec9eacc7f373b2c1cbd0e3bc87
dafful1 <78400660+dafful1@users.noreply.github.com>
2021-09-29 23:50:41 -0400

    Add files via upload

---
+++ ArraySumDriver.java
@@ -0,0 +1,30 @@
+/*
+ * Dan Afful
+ * CMSC 204
+ * Recursion Lab
+ * */
+public class ArraySumDriver
+{
+    private final static int ARRAY_SIZE=6;
+
+    public static void main(String [] args)
+    {
+        int index=0;
+        Integer [] myArray=new Integer[ARRAY_SIZE];
+        ArraySum arraySum=new ArraySum();
+
+        myArray[index++]=6;
+        myArray[index++]=5;
+        myArray[index++]=5;
+        myArray[index++]=1;
+
+        int sum=arraySum.sumOfArray(myArray,3);
+        System.out.println(sum);
+
+        myArray[index++]=7;
+        myArray[index++]=1;
+
+        sum=arraySum.sumOfArray(myArray,5);
+        System.out.println(sum);
+    }
+}
